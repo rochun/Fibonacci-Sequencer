2f7f3fdd5999b34fbde727ffe85bb864793aa845
rochun <royce.h.chun@gmail.com>
2023-08-03 10:19:01 -0700

    add back button functionality to output page and api endpoint to axios call

--- app/src/views/FibInput.tsx
+++ app/src/views/FibInput.tsx
@@ -10,7 +10,7 @@ export const FibInput = () => {
     e.preventDefault();
 
     try {
-      const response  = await axios.post('/fibonacci', {
+      const response  = await axios.post('http://localhost:3030/fibonacci', {
         n: nthInput
       })
 
@@ -30,7 +30,7 @@ export const FibInput = () => {
           Enter a number (n) to generate the first n-th Fibonacci Numbers:
           <input type="number" value={nthInput} min="1" onChange={(e) => setNthInput(e.target.value)} required />
         </label>
-        <button type='submit' style={{backgroundColor: 'grey'}} >Generate</button>
+        <button type='submit' style={{backgroundColor: 'grey'}}>Generate</button>
       </form>
     </>
   );
--- app/src/views/FibOutput.tsx
+++ app/src/views/FibOutput.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export const FibOutput = () => {
   const { n } = useParams();
-
+  const navigate = useNavigate();
   const [fibNumbers, setFibNumbers] = useState([]);
 
 
   useEffect(() => {
     const getFibNumbers = async () => {
       try {
-        const response = await axios.get(`/fibonacci/${n}`);
+        const response = await axios.get(`http://localhost:3030/fibonacci/${n}`);
 
         setFibNumbers(response.data);
       } catch (error) {
@@ -21,10 +22,15 @@ export const FibOutput = () => {
 
     getFibNumbers();
   }, [n])
+
+  const handleBack = () => {
+    navigate('/');
+  }
   return (
     <>
       <h1>First {n} Fibonacci Numbers:</h1>
       <p>{fibNumbers.join(', ')}</p>
+      <button onClick={handleBack} style={{backgroundColor: 'grey'}}>Back</button>
     </>
   );
 };
